a6436a0465f14e73c7d5b1a93797d2e9e333a91b
rachi2006 <rachirachith8@gmail.com>
2025-12-07 12:01:02 +0530

    Add user input for name and age

---
+++ 1st.py
@@ -0,0 +1,6 @@
+#userinput
+name = input("enter your name:") #input name
+print("your name is : ", name)
+age = input("enter your age >> ") #input age
+print ("age is >> ", age)
+print("your name and ,age is >>", name, age) #print both name and age 
