e5bea3de6979e6f1449691e1fedd515a8c4a2307
rachi2006 <rachirachith8@gmail.com>
2025-12-07 12:01:40 +0530

    Add basic arithmetic operations and type conversions

---
+++ 2nd.py
@@ -0,0 +1,26 @@
+#Basic arithmetic operations.
+a = 10
+b = 20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a//b)
+print(a**b)
+
+#swapping value of a and b.
+a, b = b, a
+print("a =", a)
+print("b =",  b)
+
+#typeconversion
+x = "10"
+y = int(x)
+z  = float(y)
+print(z)
+
+#type checking
+print(type(x))
+print(type(y))
+print(type(z))
